260d70f83cca24c810683f85012e04829a824264
uppitss <uppitss@gmail.com>
2022-08-25 15:28:30 +0300

    Кроппер заработал

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
+https://www.npmjs.com/package/react-cropper
+https://fengyuanchen.github.io/cropperjs/
+https://github.com/fengyuanchen/cropperjs#scalexscalex
+
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,132 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useRef, useState} from 'react';
+import Cropper from "react-cropper";
+import "cropperjs/dist/cropper.css";
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [src, setSrc] = useState("https://raw.githubusercontent.com/roadmanfong/react-cropper/master/example/img/child.jpg")
+    const [rotateTo, setRotateTo] = useState(0)
+    const [zoom, setZoom] = useState<number | undefined>(undefined)
+
+    const [croppedImage, setCroppedImage] = useState<string | undefined>(undefined);
+
+
+
+    const cropperRef = useRef<HTMLImageElement>(null);
+
+    const getCropper = () => {
+        const imageElement: any = cropperRef?.current;
+        const cropper: any = imageElement?.cropper;
+        return cropper
+    }
+
+    useEffect(() => {
+        getCropper().rotateTo(rotateTo)
+    }, [rotateTo])
+
+    useEffect(() => {
+        if (zoom !== undefined) {
+            getCropper().zoomTo(zoom)
+        }
+    }, [zoom])
+
+
+    // const onCrop = () => {
+    //     const imageElement: any = cropperRef?.current;
+    //     const cropper: any = imageElement?.cropper;
+    //     setCroppedImage(cropper.getCroppedCanvas().toDataURL())
+    // };
+
+    const countImageSize = () =>{
+        if (croppedImage !== undefined){
+
+            return ((croppedImage.length * 6 - "data:image/png;base64,".length * 8) / 8)/1024
+
+            // const stringLength = croppedImage.length - 'data:image/png;base64,'.length;
+            // const sizeInBytes = 4 * Math.ceil((stringLength / 3))*0.5624896334383812;
+            // return sizeInBytes/1024;
+        }
+        else {
+            return undefined
+        }
+    }
+
+    return (
+        <>
+            <div className={"preview_container"}>
+                <div className={"preview_image"}>
+                    <Cropper
+                        src={src}
+                        style={{height: 400, width: "100%"}}
+                        // Cropper.js options
+                        initialAspectRatio={16 / 9}
+                        guides={true}
+                        //crop={onCrop}
+                        ref={cropperRef}
+                    />
+                </div>
+                <div className={"preview_code"}>
+                    <div className={"preview_container"} style={{padding: "20px"}}>
+                        <div className={"button"}>
+                            <input type={"button"} onClick={() => {
+                                setRotateTo(prev => prev - 10)
+
+                            }} value={"Вращать влево"}/>
+                        </div>
+                        <div className={"button"}>
+                            <input type={"button"} onClick={() => {
+                                setRotateTo(prev => prev + 10)
+
+                            }} value={"Вращать вправо"}/>
+                        </div>
+
+                        <div className={"button"}>
+                            <input type={"button"} onClick={() => {
+                                setZoom(prev => (prev !== undefined ? prev : 0) + 0.1)
+                            }} value={"Zoom +"}/>
+                        </div>
+                        <div className={"button"}>
+                            <input type={"button"} onClick={() => {
+                                setZoom(prev => (prev !== undefined ? prev : 1) - 0.1)
+                            }} value={"Zoom -"}/>
+                        </div>
+                        <div className={"button"}>
+                            <input type={"file"}
+                                   accept="image/png, image/gif, image/jpeg"
+                                   onChange={(e) => {
+                                       if (e.target.files && e.target.files[0]) {
+                                           const url = URL.createObjectURL(e.target.files[0]);
+                                           setSrc(url);
+                                       }
+                                   }}/>
+                        </div>
+
+                        <div className={"button"}>
+                            <input type={"button"}
+                                   onClick={()=>{
+                                       setCroppedImage(getCropper().getCroppedCanvas().toDataURL())
+                                   }} value={"Применить"} />
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className={"preview_container"}>
+                <div className={"preview_image"}>
+                    <img src={croppedImage} width={"100%"}/>
+                </div>
+                <div className={"preview_code"}>
+                    <div>
+                        Размер: {countImageSize()} кБ
+                    </div>
+                    <br />
+                    <textarea style={{width: "100%", height: "100%"}} value={croppedImage}/>
+                </div>
+            </div>
+
+        </>
+
+    );
 }
 
 export default App;
